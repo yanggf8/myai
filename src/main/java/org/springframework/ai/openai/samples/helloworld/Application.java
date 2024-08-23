bdd74412c26b65992608c96f0c1e8791844c8c27
yanggf8 <yanggf@msn.com>
2024-08-23 15:54:47 +0800

    Init

---
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
@@ -0,0 +1,16 @@
+package org.springframework.ai.openai.samples.helloworld;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
