9b4f57704689c5cb9c2b4008364fc3761e66d3a9
yanggf8 <yanggf@msn.com>
2024-11-11 10:26:18 +0800

    change

--- src/main/java/org/springframework/ai/openai/samples/helloworld/Config.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/Config.java
@@ -1,15 +1,25 @@
 package org.springframework.ai.openai.samples.helloworld;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class Config {
 
+    @Value("${openai.base-url}")
+    private String baseUrl;
+
+    @Value("${openai.model}")
+    private String model;
+
     @Bean
     ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
+        return builder
+                .baseUrl(baseUrl)
+                .model(model)
+                .build();
     }
 
 }
